54b3abffc85ae5326363ce3529e07aa1cbcde3d9
Shiva <shivaprasadjayebaye@gmail.com>
2026-07-06 20:59:38 +0530

    Added Calculator1 program

---
+++ src/Calculator1.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Calculator1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Calculator1 cal = new Calculator1();
+
+        int num1, num2, operation;
+        char choice = 'Y';
+
+        while (choice == 'Y' || choice == 'y') {
+
+            System.out.println("======");
+            System.out.print("Enter First Number : ");
+            num1 = sc.nextInt();
+
+            System.out.print("Enter Second Number : ");
+            num2 = sc.nextInt();
+
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+
+            System.out.print("Choose Operation : ");
+            operation = sc.nextInt();
+
+            cal.calculate(num1, num2, operation);
+
+            System.out.print("Do you want to continue? (Y/N) : ");
+            choice = sc.next().charAt(0);
+        }
+
+        System.out.println("==Thank You==");
+        sc.close();
+    }
+
+    void calculate(int a, int b, int operation) {
+        switch (operation) {
+
+            case 1:
+                System.out.println("Answer = " + (a + b));
+                break;
+
+            case 2:
+                System.out.println("Answer = " + (a - b));
+                break;
+
+            case 3:
+                System.out.println("Answer = " + (a * b));
+                break;
+
+            case 4:
+                if (b == 0) {
+                    System.out.println("Division by Zero is not Possible.");
+                } else {
+                    System.out.println("Answer = " + (a / b));
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Choice! Please choose b/w 1 to 4.");
+        }
+    }
+}
