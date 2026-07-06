cd367636ed4f36b55774ac3077c901b6f15ff481
sukanyakilari1992-ui <sukanyakilari1992@gmail.com>
2026-07-06 19:55:03 +0530

    Update Main.java

--- oopscalcaualtor/src/Main.java
+++ oopscalcaualtor/src/Main.java
@@ -1,86 +1,95 @@
 import java.util.Scanner;
-
-// Abstraction
 abstract class Calculator {
     abstract int calculate(int a, int b);
     abstract String getOperation();
 }
-
 // Encapsulation
 class Numbers {
     private int num1;
     private int num2;
-
-    public void setNum1(int num1) {
+    public void setNum1(int num1) 
+    {
         this.num1 = num1;
     }
-
-    public void setNum2(int num2) {
+    public void setNum2(int num2) 
+    {
         this.num2 = num2;
     }
-
-    public int getNum1() {
+    public int getNum1() 
+    {
         return num1;
     }
-
-    public int getNum2() {
+    public int getNum2() 
+    {
         return num2;
     }
 }
 
 // Inheritance
-class Addition extends Calculator {
-    int calculate(int a, int b) {
+class Addition extends Calculator 
+{
+    int calculate(int a, int b) 
+    {
         return a + b;
     }
-
-    String getOperation() {
+    String getOperation() 
+    {
         return "Addition";
     }
 }
 
-class Subtraction extends Calculator {
-    int calculate(int a, int b) {
+class Subtraction extends Calculator 
+{
+    int calculate(int a, int b) 
+    {
         return a - b;
     }
 
-    String getOperation() {
+    String getOperation() 
+    {
         return "Subtraction";
     }
 }
 
-class Multiplication extends Calculator {
-    int calculate(int a, int b) {
+class Multiplication extends Calculator 
+{
+    int calculate(int a, int b) 
+    {
         return a * b;
     }
 
-    String getOperation() {
+    String getOperation() 
+    {
         return "Multiplication";
     }
 }
 
-class Division extends Calculator {
-    int calculate(int a, int b) {
-        if (b == 0) {
+class Division extends Calculator 
+{
+    int calculate(int a, int b) 
+    {
+        if (b == 0) 
+        {
             System.out.println("Division by zero is not possible.");
             return 0;
         }
         return a / b;
     }
 
-    String getOperation() {
+    String getOperation() 
+    {
         return "Division";
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         Numbers n = new Numbers();
-
-        while (true) {
-
+        while (true) 
+        {
             System.out.println("\n===== Calculator =====");
             System.out.println("1. Addition");
             System.out.println("2. Subtraction");
@@ -90,8 +99,9 @@ public class Main {
 
             System.out.print("Enter your choice: ");
             int choice = sc.nextInt();
-
-            if (choice == 5) {
+            
+            if (choice == 5) 
+            {
                 System.out.println("Calculator Closed.");
                 break;
             }
@@ -103,28 +113,17 @@ public class Main {
             n.setNum2(sc.nextInt());
 
             Calculator cal;
-
-            switch (choice) {
-                case 1:
-                    cal = new Addition();
-                    break;
-
-                case 2:
-                    cal = new Subtraction();
-                    break;
-
-                case 3:
-                    cal = new Multiplication();
-                    break;
-
-                case 4:
-                    cal = new Division();
-                    break;
-
-                default:
+            cal = switch (choice) 
+            {
+                case 1 -> new Addition();
+                case 2 -> new Subtraction();
+                case 3 -> new Multiplication();
+                case 4 -> new Division();
+                default -> {
                     System.out.println("Invalid Choice");
                     continue;
-            }
+                }
+            };
 
             int result = cal.calculate(n.getNum1(), n.getNum2());
 
