de4823cf2c88fd280d1611cae690c50d4bfa0e20
srijaporalla <srijaporalla@gmail.com>
2026-07-23 10:52:54 +0100

    Update and rename calculator.java to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class calculator {
+public class Calculator {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("enter number 1:");
@@ -8,10 +8,10 @@ public class calculator {
         double s2=sc.nextDouble();
         System.out.println("enter  operator( +, -, *, /, %):");
         char operator=sc.next().charAt(0);
-        calculator obj=new calculator();
-        obj.calculator(s1,s2,operator);
+        Calculator obj=new Calculator();
+        obj.Calculator(s1,s2,operator);
     }
-    public void calculator(double s1,double s2,char operator){
+    public void Calculator(double s1,double s2,char operator){
         if(operator=='*'){
             System.out.println("Result : "+(s1*s2)); 
         }else if(operator=='+'){
